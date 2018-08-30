50fdcf9b30c92b4f70a79a42b5cdee0988b39499
JerryChan123 <linchen0922@163.com>
2018-08-30 23:07:12 +0800

    修复std::cout找不到定义问题

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -35,6 +35,8 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
+'isystem',
+'/usr/include/c++/7.3.0' #这个一定要在 /usr/include前面
 '-isystem',
 '/usr/include',
 '-isystem',
